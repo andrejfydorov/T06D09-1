484f0210db87144a015f53241324bd33423b2284
andrejfydorov <andrejfydorov@mail.ru>
2023-03-15 18:30:12 +0300

    modified key9part1.c

--- src/fast_sort.c
+++ src/fast_sort.c
@@ -9,7 +9,7 @@ void quick_sort(int *s_arr, int first, int last);
 int check_array(int *a, int n);
 void swap(int *a, int *b);
 void siftDown(int *numbers, int root, int bottom);
-void heapSort(int *numbers, int array_size) ;
+void heapSort(int *numbers, int array_size);
 
 int main() {
   int n = NMAX, data[NMAX];
@@ -90,45 +90,38 @@ void swap(int *a, int *b) {
   *b = temp;
 }
 // Функция "просеивания" через кучу - формирование кучи
-void siftDown(int *numbers, int root, int bottom)
-{
+void siftDown(int *numbers, int root, int bottom) {
   int maxChild; // индекс максимального потомка
   int done = 0; // флаг того, что куча сформирована
   // Пока не дошли до последнего ряда
-  while ((root * 2 <= bottom) && (!done)) 
-  {
-    if (root * 2 == bottom)    // если мы в последнем ряду,
-      maxChild = root * 2;    // запоминаем левый потомок
+  while ((root * 2 <= bottom) && (!done)) {
+    if (root * 2 == bottom) // если мы в последнем ряду,
+      maxChild = root * 2;  // запоминаем левый потомок
     // иначе запоминаем больший потомок из двух
     else if (numbers[root * 2] > numbers[root * 2 + 1])
       maxChild = root * 2;
     else
       maxChild = root * 2 + 1;
     // если элемент вершины меньше максимального потомка
-    if (numbers[root] < numbers[maxChild]) 
-    {
+    if (numbers[root] < numbers[maxChild]) {
       int temp = numbers[root]; // меняем их местами
       numbers[root] = numbers[maxChild];
       numbers[maxChild] = temp;
       root = maxChild;
-    }
-    else // иначе
+    } else      // иначе
       done = 1; // пирамида сформирована
   }
 }
 // Функция сортировки на куче
-void heapSort(int *numbers, int array_size) 
-{
+void heapSort(int *numbers, int array_size) {
   // Формируем нижний ряд пирамиды
   for (int i = (array_size / 2); i >= 0; i--)
     siftDown(numbers, i, array_size - 1);
   // Просеиваем через пирамиду остальные элементы
-  for (int i = array_size - 1; i >= 1; i--)
-  {
+  for (int i = array_size - 1; i >= 1; i--) {
     int temp = numbers[0];
     numbers[0] = numbers[i];
     numbers[i] = temp;
     siftDown(numbers, 0, i - 1);
   }
 }
-
--- src/key9part1.c
+++ src/key9part1.c
@@ -5,11 +5,15 @@
 -------------------------------------*/
 
 #include <stdio.h>
+#include <ctype.h>
 
-void input (int *buffer, int *length);
+#define NMAX 10
+
+int input (int *buffer, int *length);
 void output (int *buffer, int length);
 int sum_numbers(int *buffer, int length);
-int find_numbers(int* buffer, int length, int number, int* numbers)
+int find_numbers(int* buffer, int length, int number, int* numbers);
+int check_array(int *a, int n);
 
 /*------------------------------------
 	Функция получает массив данных 
@@ -22,7 +26,42 @@ int find_numbers(int* buffer, int length, int number, int* numbers)
 -------------------------------------*/
 int main()
 {
-    
+    int n = NMAX, data[NMAX], data_out[NMAX];
+	int inp = input(data, &n);
+	int sum = sum_numbers(data, n);
+	int ch = check_array(data, n);
+
+	if (inp == 0 && sum != 0 && ch == 0) {
+		int finded = find_numbers(data, n, sum, data_out);		
+		printf("%d\n", sum);
+		output(data_out, finded);
+	} else {
+		printf("n/a");
+	}
+
+	return 0;
+}
+
+int input(int *buffer, int *length) {
+  scanf("%d", length);
+  if (*length > 10)
+	return -1;
+  for (int *p = buffer; p - buffer < *length; p++) {
+    if (scanf("%d", p) != 1) {
+      return -1;
+    }
+  }
+  return 0;
+}
+
+void output(int *a, int n) {
+  for (int *p = a; p - a < n; p++) {
+    if ((p - a) == n - 1) {
+      printf("%d\n", *p);
+    } else {
+      printf("%d ", *p);
+    }
+  }
 }
 
 /*------------------------------------
@@ -34,9 +73,9 @@ int sum_numbers(int *buffer, int length)
 {
 	int sum = 0;
 	
-	for (int i = 1; i < length; i++)
+	for (int i = 0; i < length; i++)
 	{
-		if (i % 2 != 0)
+		if (buffer[i] % 2 == 0)
 		{
 			sum = sum + buffer[i];
 		}
@@ -53,5 +92,32 @@ int sum_numbers(int *buffer, int length)
 -------------------------------------*/
 int find_numbers(int* buffer, int length, int number, int* numbers)
 {
+	int finded = 0;
+	for (int *p = buffer; p - buffer < length; p++)
+	{
+		if (*p == 0)
+		{
+			continue;
+		}	
+		if ((number % (*p)) == 0)
+		{
+			*numbers = *p;
+			numbers++;
+			finded++;
+		}
+		
+	}
+	
+	return finded;
+}
 
+int check_array(int *a, int n) {
+  int res = 0;
+  for (int *p = a; p - a < n; p++) {
+    if (isdigit(*p) != 0) {
+      return -1;
+    }
+  }
+
+  return res;
 }
